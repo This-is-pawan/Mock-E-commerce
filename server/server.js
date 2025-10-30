2dbf827aff7b3628342e6e0230c1946a6f8a16bf
Pawan <154042595+This-is-pawan@users.noreply.github.com>
2025-10-30 13:44:00 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -11,7 +11,7 @@ const app = express();
 app.use(cookieParser());
 app.use(express.json());
 app.use(cors({
-  origin:'http://localhost:5173',
+  origin:'https://mock-commerce-znii.onrender.com',
 credentials:true,
 }))
 app.use('/api',route);
