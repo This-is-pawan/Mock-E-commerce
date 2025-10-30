1d877a94ce46ff6bb0d6aebb9f6503de7508453e
Pawan <154042595+This-is-pawan@users.noreply.github.com>
2025-10-30 20:11:46 +0530

    Update AuthRoute.js

--- server/routes/AuthRoute.js
+++ server/routes/AuthRoute.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { Login, LogOut, Products, Register } from "../controller/userController.js";
+import { Login, LogOut, Products, Register ,UserExist } from "../controller/userController.js";
 import { verifyToken } from "../middleware/Auth.js";
 
 const route = express.Router();
@@ -9,6 +9,7 @@ route.post("/register", Register);
 route.post("/login", Login);
 route.post("/logout", LogOut);
 route.get("/products", verifyToken, Products);
+route.get("/user", verifyToken, UserExist);
 route.post("/add",addToCart);
 route.get("/cart-item", getCartItems);
 route.patch("/qty/:id",updateQuantity);
