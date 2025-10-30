06091a90c615443b91a3337dbe45c73c58ea3519
Pawan <154042595+This-is-pawan@users.noreply.github.com>
2025-10-30 19:40:59 +0530

    Update contextApi.jsx

--- client/MockStore/src/contextApi.jsx
+++ client/MockStore/src/contextApi.jsx
@@ -1,16 +1,33 @@
 // src/contextApi.jsx
-import React, { createContext, useState } from "react";
-
+import React, { createContext, useState, useEffect } from "react";
+import axios from "axios"
 export const AuthContext = createContext();
 
 const ContextApi = ({ children }) => {
   const [isAuth, setIsAuth] = useState(false);
+  const [userExist,setExistUser]=useState(null);
 
+const handleUserExist = async () => {
+    try {
+      const result = await axios.get("https://mock-commerce-backend.onrender.com/api/user", {
+        withCredentials: true,
+      });
+      setExistUser(result.data);
+      console.log("User data:", result.data);
+    } catch (err) {
+      console.error("Error fetching user:", err.message);
+    }
+  };
+ useEffect(() => {
+    handleUserExist();
+  }, []);
+  
   return (
-    <AuthContext.Provider value={{ isAuth, setIsAuth }}>
+    <AuthContext.Provider value={{ isAuth, setIsAuth, handleUserExist, userExist }}>
       {children}
     </AuthContext.Provider>
   );
+  
 };
 
 export default ContextApi;
